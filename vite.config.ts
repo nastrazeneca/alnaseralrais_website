d36e27e730e80c7d4b439b7e174a9a08ce5667f7
nastrazeneca <alnaser.alrais@gmail.com>
2025-10-20 00:15:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/alnaseralrais_website/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
